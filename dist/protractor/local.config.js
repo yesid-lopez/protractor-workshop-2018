b41bd273ebd512b2b41a43d4eb02b6c50b1e7751
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-16 15:53:58 -0500

    improve test

--- dist/protractor/local.config.js
+++ dist/protractor/local.config.js
@@ -1,8 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const protractor_1 = require("protractor");
 exports.config = {
     framework: 'jasmine',
     specs: ['../test/google.spec.js'],
-    seleniumAddress: 'http://localhost:4444/wd/hub'
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    onPrepare: () => {
+        protractor_1.browser.ignoreSynchronization = true;
+    }
 };
 //# sourceMappingURL=local.config.js.map
