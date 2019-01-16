0da73e01fec07fdb4092ed97d022432a1a1aa867
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-16 16:33:39 -0500

    remove folders

--- dist/protractor/local.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = {
-    framework: 'jasmine',
-    specs: ['../test/google.spec.js'],
-    seleniumAddress: 'http://localhost:4444/wd/hub'
-};
-//# sourceMappingURL=local.config.js.map
