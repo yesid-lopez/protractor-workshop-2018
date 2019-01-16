cdb984c9b26344981dd9742bbc44740f7046ab7b
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-16 15:43:50 -0500

    update dist

--- dist/test/google.spec.js
+++ dist/test/google.spec.js
@@ -5,7 +5,7 @@ describe('This is the first example of protractor', () => {
     it('should have a title', () => {
         protractor_1.browser.ignoreSynchronization = true;
         protractor_1.browser.get('http://www.google.com');
-        expect(protractor_1.browser.getTitle()).toEqual('Googlde');
+        expect(protractor_1.browser.getTitle()).toEqual('Google');
     });
 });
 //# sourceMappingURL=google.spec.js.map
