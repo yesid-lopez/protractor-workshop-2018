c2b79e856c4832c64a2b488f386a8003971ae5eb
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-17 11:55:36 -0500

    css-selector test

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -4,7 +4,7 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
