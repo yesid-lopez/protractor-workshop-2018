445aa016326e3900dfec3efb03cbba5b34d40ba3
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-17 14:02:31 -0500

    setup vs-debugger

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -6,6 +6,7 @@ export const config: Config = {
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
+  getPageTimeout: 1000,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
