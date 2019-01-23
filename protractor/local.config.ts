04609be1e90f4af830b57daabd7f74eade76c802
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-23 10:47:52 -0500

    Config capabilities

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -15,5 +15,12 @@ export const config: Config = {
   },
   jasmineNodeOpts: {
     defaultTimeoutInterval: 120000
-  }
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-popup-blocking', '--no-default-browser-check', '--window-size=800,600'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
 };
