df9d14d5954d51f8c7a0d646a815d9b8e9363ef3
Yesid Leonardo López Sierra <yesid.lopez@correo.icesi.edu.co>
2019-01-23 09:30:00 -0500

    Css selector (#9)
    * improve protractor
    
    * add headless configuration
    
    * add travis configuration
    
    * add dependencies
    
    * fix code
    
    * setup vs-debugger
    
    * css-selector test

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -4,8 +4,9 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
+  getPageTimeout: 1000,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
