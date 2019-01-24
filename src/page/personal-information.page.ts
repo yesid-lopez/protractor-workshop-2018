01abc049694fa3b9527b7c3c6ef69c72aca0504d
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-24 14:25:33 -0500

    add changes

--- src/page/personal-information.page.ts
+++ src/page/personal-information.page.ts
@@ -1,29 +1,77 @@
-import { ElementFinder, ElementArrayFinder, $$ } from 'protractor';
+import { ElementFinder, $, $$, ElementArrayFinder  } from 'protractor';
 
 export class PersonalInformationPage {
-  private form: ElementArrayFinder;
+
+  private firstNameInput: ElementFinder;
+  private lastNameInput: ElementFinder;
+  private sexRadioButton: ElementArrayFinder;
+  private toolsCheckbox: ElementArrayFinder;
+  private experienceRadioButton: ElementArrayFinder;
+  private professionCheckbox: ElementArrayFinder;
+  private continentSelectOne: ElementArrayFinder;
+  private commandsSelectMultiple: ElementArrayFinder;
+
+  private buttonButton:ElementFinder;
+  private confirmationText:ElementFinder;
+
   constructor() {
-    this.form = $$('form-horizontal');
+    this.firstNameInput = $('[name="firstname"]');
+    this.lastNameInput = $('[name="lastname"]');
+    this.sexRadioButton = $$('[name="sex"]');
+    this.experienceRadioButton = $$('[name="exp"]');
+    this.professionCheckbox = $$('[name="profession"]');
+    this.toolsCheckbox = $$('[name="tool"]');
+    this.continentSelectOne = $$('#continents option');
+    this.commandsSelectMultiple = $$('#selenium_commands option');
+    this.buttonButton = $('#submit');
   }
-  public findElementByName(valueField:string): ElementFinder {
-    return this.form.filter((element:ElementFinder) => {
-      return element.$('input').getAttribute('type').then((valueType:string) => {
-        let condition:boolean = false;
-        if (valueType === 'text') {
-          element.getText().then(valueText => condition = valueText ===  valueField);
-        } else {
-          if (valueType === 'checkbox') {
-            element.getAttribute('value')
-            .then(valueText => condition = valueText ===  valueField);
-          } else {
-            if (valueType === 'radio') {
-              element.getAttribute('value')
-              .then(valueText => condition = valueText ===  valueField);
-            }
-          }
-        }
-        return condition;
-      });
-    }).first();
+
+  public async fillForm(data : {
+    firstName: string,
+    lastName: string,
+    sex: string,
+    experience: number,
+    profession: string[],
+    tools: string[],
+    continent: string,
+    commands: string[] }):Promise<void> {
+    await this.fillInputs(data.firstName, data.lastName);
+    await this.fillRadioButtons(data.sex, data.experience);
+    await this.fillCheckBoxes(data.tools, data.profession);
+    await this.continentSelectOne.filter(element =>
+      element.getText().then(text => text === data.continent)).click();
+    await this.commandsSelectMultiple.filter(element =>
+      element.getText().then(text =>
+        data.commands.some(command => command === text))).click();
+    await this.buttonButton.click();
   }
+  private async fillCheckBoxes(tools:string[], professions:string[]):Promise<void> {
+    await this.toolsCheckbox.filter(element =>
+      element.getAttribute('value').then(
+        name => tools.some(tool => tool === name))).click();
+
+    await this.professionCheckbox.filter(element =>
+      element.getAttribute('value').then(
+        name => professions.some(profession => profession === name))).click();
+  }
+
+  private async fillRadioButtons(sex:string, experience:number):Promise<void> {
+    await this.sexRadioButton.filter(element =>
+      element.getAttribute('value').then(name => name === sex)
+      ).first().click();
+    await this.experienceRadioButton.filter(element =>
+      element.getAttribute('value').then(name => Number(name) === experience)
+      ).first().click();
+  }
+
+  private async fillInputs(firstName:string, lastName:string):Promise<void> {
+    await this.firstNameInput.sendKeys(firstName);
+    await this.lastNameInput.sendKeys(lastName);
+  }
+
+  public async getConfirmationText():Promise<string> {
+    this.confirmationText = $('.wpb_wrapper h1');
+    return await this.confirmationText.getText();
+  }
+
 }
--- test/locators.spec.ts
+++ test/locators.spec.ts
@@ -1,7 +1,32 @@
-import {} from 'protractor';
+import { browser } from 'protractor';
+import { PersonalInformationPage } from '../src/page';
 
-describe('When you want to fill a form', () => {
+describe('Given an automation practice form', () => {
   beforeAll(async () => {
-
+    await browser.get('http://toolsqa.com/automation-practice-form/');
+  });
+  describe('When you want to fill a personal information form', () => {
+    beforeAll(async () => {
+      const personalInformationPage = new PersonalInformationPage();
+      await personalInformationPage.fillForm({
+        firstName: 'Alejandro',
+        lastName: 'Perdomo',
+        sex: 'Male',
+        experience: 7,
+        profession: ['Automation Tester'],
+        tools: ['Selenium Webdriver'],
+        continent: 'South America',
+        commands: [
+          'Browser Commands',
+          'Navigation Commands',
+          'Switch Commands',
+          'Wait Commands',
+          'WebElement Commands']
+      });
+    });
+    it('then you sould send the information', async () => {
+      const personalInformationPage = new PersonalInformationPage();
+      expect(personalInformationPage.getConfirmationText()).toBe('Practice Automation Form');
+    });
   });
 });
