cb101d4e3dea712fe55af2820b76c2d89a7f3db4
Yesid Leonardo Lopez Sierra <ylopezs@pslcol.com.co>
2019-01-25 17:54:49 -0500

    include more browsers

--- protractor/saucelabs.config.ts
+++ protractor/saucelabs.config.ts
@@ -2,7 +2,25 @@ import { Config, browser } from 'protractor';
 
 import { reporter } from './helpers/reporter';
 
+const firefoxConfig = {
+  browserName: 'firefox',
+  platform: 'linux',
+  name: 'firefox-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const chromeConfig = {
+  browserName: 'chrome',
+  name: 'chrome-tests',
+  shardTestFiles: true,
+  maxInstances: 1
+};
+
+const multiCapabilities = [chromeConfig, firefoxConfig];
+
 export const config: Config = {
+  multiCapabilities,
   framework: 'jasmine',
   specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER:false,
